cd4dc15d8f1ea1556f633ecd107027830f3ce163
stevenleggdfe <51697598+stevenleggdfe@users.noreply.github.com>
2022-04-28 17:36:15 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,21 +7,35 @@ const testTable1 = require ("./includes/test_table_counting_apply_events_today")
 const testTable2 = require ("./includes/test_table_counting_apply_events_today2");
 
 module.exports = (params) => {
+    
+ params = {
+    bqProjectName: null, // name of the BigQuery project that dfe-analytics streams event data into
+    bqDatasetName: null, // name of the BigQuery dataset that dfe-analytics streams event data into
+    bqEventsTableName: 'events', // name of the BigQuery table that dfe-analytics streams event data into
+    ...params
+  };
 
-    params = {
-      // TODO: set default params
+  const {
+    defaultConfig,
+    bqProjectName,
+    bqDatasetName,
+    bqEventsTableName
+  } = params;
+  
+  // Declare the source table
+  const eventsRaw = declare({
+    ...defaultConfig,
+    database: bqProjectName,
+    schema: bqDatasetName,
+    name: bqEventsTableName
+  });
 
-      ...params
-    };
+  // Publish and return datasets.
 
-    // Publish and return datasets.
-    let result = {
-        testTable1: testTable1(params),
-        testTable2: testTable2(params)
-    // TODO: update files to call with params
-    //   file_one: file_one(params),
-    //   file_two: file_two(params)
-    };
-
-    return result;
+  return {
+      eventsRaw,
+      testTable1: testTable1(params),
+      testTable2: testTable2(params),
+      dataFunctions
+  }
 }
