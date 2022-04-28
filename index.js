d7997298989fdb02f88491f4490261dfff0b29ce
stevenleggdfe <51697598+stevenleggdfe@users.noreply.github.com>
2022-04-28 17:15:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 // const file_one = require("./includes/file_one");
 // const file_two = require("./includes/file_two");
 
+const dataFunctions = require("./includes/data_functions");
+const testTable1 = require ("./includes/test_table_counting_apply_events_today");
+const testTable2 = require ("./includes/test_table_counting_apply_events_today2");
+
 module.exports = (params) => {
 
     params = {
@@ -12,6 +16,8 @@ module.exports = (params) => {
 
     // Publish and return datasets.
     let result = {
+        testTable1: testTable1(params),
+        testTable2: testTable2(params)
     // TODO: update files to call with params
     //   file_one: file_one(params),
     //   file_two: file_two(params)
