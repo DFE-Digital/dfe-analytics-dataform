8401fdc04205b131903a7a006001dba006d96b2c
stevenleggdfe <51697598+stevenleggdfe@users.noreply.github.com>
2022-04-28 16:31:22 +0100

    Update entity_crud_processing.js

--- includes/entity_crud_processing.js
+++ includes/entity_crud_processing.js
@@ -3,6 +3,6 @@ module.exports = (params) => {
   return publish("test_table_counting_apply_events_today", {
     ...params.defaultConfig
   }).query(ctx => `
-SELECT COUNT(*) FROM ${ctx.ref("apply_events_production","events")} WHERE DATE(occurred_at) = CURRENT_DATE
+SELECT COUNT(*) AS count FROM ${ctx.ref("apply_events_production","events")} WHERE DATE(occurred_at) = CURRENT_DATE
 `)
 }
