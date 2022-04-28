ba2ddf607d3f4263ec901bf2e0ece7b5161beb40
stevenleggdfe <51697598+stevenleggdfe@users.noreply.github.com>
2022-04-28 17:11:25 +0100

    Update test_table_counting_apply_events_today2.js

--- includes/test_table_counting_apply_events_today2.js
+++ includes/test_table_counting_apply_events_today2.js
@@ -3,6 +3,6 @@ module.exports = (params) => {
   return publish("test_table_counting_apply_events_today2", {
     ...params.defaultConfig
   }).query(ctx => `
-SELECT SUM(*) AS sum FROM ${ctx.ref("test_table_counting_apply_events_today")} WHERE DATE(occurred_at) = CURRENT_DATE
+SELECT SUM(*) AS sum FROM ${ctx.ref("test_table_counting_apply_events_today")}
 `)
 }
