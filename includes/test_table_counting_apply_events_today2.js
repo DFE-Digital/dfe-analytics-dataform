5b727d79e2671a94a24bffc5bcdc8b4479ede8c7
stevenleggdfe <51697598+stevenleggdfe@users.noreply.github.com>
2022-04-28 17:44:41 +0100

    Update test_table_counting_apply_events_today2.js

--- includes/test_table_counting_apply_events_today2.js
+++ includes/test_table_counting_apply_events_today2.js
@@ -1,6 +1,6 @@
 module.exports = (params) => {
   
-  return publish("test_table_counting_apply_events_today2", {
+  return publish("test_table_counting_apply_events_today2"+params.tableSuffix, {
     ...params.defaultConfig
   }).query(ctx => `
 SELECT SUM(*) AS sum FROM ${ctx.ref("test_table_counting_apply_events_today")}
