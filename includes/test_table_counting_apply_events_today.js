c86d075d99f39013f0f7cad954826b8307824ab4
stevenleggdfe <51697598+stevenleggdfe@users.noreply.github.com>
2022-04-28 17:58:30 +0100

    Update test_table_counting_apply_events_today.js

--- includes/test_table_counting_apply_events_today.js
+++ includes/test_table_counting_apply_events_today.js
@@ -3,6 +3,6 @@ module.exports = (params) => {
   return publish("test_table_counting_apply_events_today_"+params.tableSuffix, {
     ...params.defaultConfig
   }).query(ctx => `
-SELECT COUNT(*) AS count FROM ${ctx.ref("apply_events_production","events")} WHERE DATE(occurred_at) = CURRENT_DATE
+SELECT COUNT(*) AS count FROM ${ctx.ref(params.bqDatasetName,params.bqEventsTableName)} WHERE DATE(occurred_at) = CURRENT_DATE
 `)
 }
