8bb6b0940fdd66f0d69816c6a81b9fb837455960
stevenleggdfe <51697598+stevenleggdfe@users.noreply.github.com>
2022-04-28 17:02:28 +0100

    Update and rename entity_crud_processing.js to test_table_counting_apply_events_today.js

--- includes/test_table_counting_apply_events_today.js
+++ includes/test_table_counting_apply_events_today.js
@@ -1,10 +1,5 @@
 module.exports = (params) => {
   
-  return publish("test_table_counting_apply_events_today2", {
-    ...params.defaultConfig
-  }).query(ctx => `
-SELECT COUNT(*) AS count2 FROM ${ctx.ref("apply_events_production","events")} WHERE DATE(occurred_at) = CURRENT_DATE
-`);
   return publish("test_table_counting_apply_events_today", {
     ...params.defaultConfig
   }).query(ctx => `
