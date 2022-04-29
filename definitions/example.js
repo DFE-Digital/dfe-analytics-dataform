f214518b9043169f773af23240a3f123013c0852
Steven Legg <google-oauth2|105490946821392461153>
2022-04-29 10:52:27 +0000

    Update example.js

--- definitions/example.js
+++ definitions/example.js
@@ -1,7 +1,9 @@
-// TODO: update package name
-const package = require("../");
+const dfeAnalyticsDataform = require("../"); // change ../ to dfe-analytics-dataform when using this in your own Dataform project
 
-const models = package({
-  // TODO: add variables
+// Repeat the lines below for each and every events table you want dfe-analytics-dataform to process in your Dataform project
+dfeAnalyticsDataform({
+    tableSuffix: "Your table suffix here",
+    bqProjectName: "Your BigQuery project name here",
+    bqDatasetName: "Your BigQuery dataset name here",
+    bqEventsTableName: "Your BigQuery events table name here - usually just 'events'"
 });
-
