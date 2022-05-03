aff22eb5c9c3a14893e60debda1503df8e0585ab
Steven Legg <google-oauth2|105490946821392461153>
2022-05-03 11:41:21 +0000

    Fix test include

--- includes/test_table_counting_events_today2.js
+++ includes/test_table_counting_events_today2.js
@@ -3,6 +3,6 @@ module.exports = (params) => {
   return publish("test_table_counting_"+params.tableSuffix+"_events_today2", {
     ...params.defaultConfig
   }).query(ctx => `
-SELECT SUM(*) AS sum FROM ${ctx.ref("test_table_counting_"+params.tableSuffix+"_events_today")}
+SELECT SUM(count) AS sum FROM ${ctx.ref("test_table_counting_"+params.tableSuffix+"_events_today")}
 `)
 }
