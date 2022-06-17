39ecd9ee7efc3877893229850fb2a3894e71abd1
Steven Legg <google-oauth2|105490946821392461153>
2022-06-17 09:24:40 +0000

    Small fix for ambiguity issue

--- includes/events.js
+++ includes/events.js
@@ -65,7 +65,8 @@ module.exports = (params) => {
   WHERE
     event_type = "web_request"
     /* Process web requests as far back as 1 day before the timestamp we're updating this table from, to ensure that we do find the web request for each non-web request event, even if the non-web request event occurred the other side of event_timestamp_checkpoint from the web request event that caused it */
-    AND occurred_at > TIMESTAMP_SUB(event_timestamp_checkpoint, INTERVAL 1 DAY)
+    AND minimal_earliest_event_for_web_request.occurred_at > TIMESTAMP_SUB(event_timestamp_checkpoint, INTERVAL 1 DAY)
+    AND web_request.occurred_at > TIMESTAMP_SUB(event_timestamp_checkpoint, INTERVAL 1 DAY)
   GROUP BY request_uuid
 )
 SELECT
