0a67db0680878e1b8817626503e11dcc3dc77382
Steven Legg <google-oauth2|105490946821392461153>
2022-07-21 11:15:49 +0000

    Add entity_at operations

---
+++ includes/entity_at.js
@@ -0,0 +1,32 @@
+module.exports = (params) => {
+  return params.dataSchema.forEach(tableSchema => operate(tableSchema.entityTableName + '_at_' + params.eventSourceName).queries(ctx => `
+  CREATE OR REPLACE TABLE FUNCTION \`${params.bqProjectName}.${params.bqDatasetName}.${tableSchema.entityTableName}_at_${params.eventSourceName}\`(timestamp_at TIMESTAMP)
+  AS
+  SELECT
+    * EXCEPT (valid_from,
+      valid_to,
+      event_type,
+      request_uuid,
+      request_path,
+      request_user_id,
+      request_method,
+      request_user_agent,
+      request_referer,
+      request_query,
+      response_content_type,
+      response_status,
+      anonymised_user_agent_and_ip,
+      device_category,
+      browser_name,
+      browser_version,
+      operating_system_name,
+      operating_system_vendor,
+      operating_system_version)
+  FROM
+    ${ctx.ref(tableSchema.entityTableName + "_version_" + params.eventSourceName)}
+  WHERE
+    (valid_to IS NULL
+      OR valid_to > CURRENT_TIMESTAMP)
+    AND valid_from <= CURRENT_TIMESTAMP
+  `))
+}
