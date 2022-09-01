e7d03890036493e4bb1637484a2f7fbc03da1baf
Steven Legg <google-oauth2|105490946821392461153>
2022-09-01 13:41:41 +0000

    Auto-detect data types in data_schema_json_latest

--- includes/data_schema_json_latest.js
+++ includes/data_schema_json_latest.js
@@ -17,24 +17,47 @@ module.exports = (params) => {
 FROM (
   SELECT
     entity_table_name,
-    "{\\n   entityTableName: \\"" || entity_table_name || "\\",\\n   description: \\"\\",\\n   keys: [" || STRING_AGG("{\\n      keyName: \\"" || key || "\\",\\n      dataType: \\"string\\",\\n      description: \\"\\"\\n   }", ", ") || "]\\n}" AS tableSchemaJSON
+    "{\\n   entityTableName: \\"" || entity_table_name || "\\",\\n   description: \\"\\",\\n   keys: [" || STRING_AGG("{\\n      keyName: \\"" || key || "\\",\\n      dataType: \\"" || data_type || "\\",\\n      description: \\"\\"\\n   }", ", ") || "]\\n}" AS tableSchemaJSON
   FROM (
     SELECT
-      DISTINCT entity_table_name,
-      DATA.key AS key
-    FROM
-      ${ctx.ref("events_" + params.eventSourceName)},
-      UNNEST(DATA) AS DATA
-    WHERE
-      DATE(occurred_at) = CURRENT_DATE - 1
-      AND event_type IN ("create_entity",
-        "update_entity",
-        "delete_entity",
-        "import_entity")
-      AND key NOT IN ("created_at","updated_at", "id")
-    ORDER BY
       entity_table_name,
-      key)
+      key,
+      /* If we identified only one non-null data type for this field, use that data type - otherwise default to string */
+      IF(
+          COUNT(DISTINCT data_type) = 1,
+          ANY_VALUE(data_type),
+          "string"
+        ) AS data_type
+    FROM (
+      SELECT
+        DISTINCT entity_table_name,
+        DATA.key AS key,
+      CASE
+        /* Attempt to work out the data type(s) of each field, in this priority order */
+        WHEN DATA.value[SAFE_OFFSET(0)] IS NULL THEN NULL
+        WHEN SAFE_CAST(DATA.value[SAFE_OFFSET(0)] AS BOOL) IS NOT NULL THEN "boolean"
+        WHEN SAFE_CAST(DATA.value[SAFE_OFFSET(0)] AS INT64) IS NOT NULL THEN "integer"
+        WHEN SAFE_CAST(DATA.value[SAFE_OFFSET(0)] AS FLOAT64) IS NOT NULL THEN "float"
+        WHEN ${data_functions.stringToTimestamp(`DATA.value[SAFE_OFFSET(0)]`)} IS NOT NULL THEN "timestamp"
+        WHEN ${data_functions.stringToDate(`DATA.value[SAFE_OFFSET(0)]`)} IS NOT NULL THEN "date"
+        WHEN REGEXP_CONTAINS(DATA.value[SAFE_OFFSET(0)], r"^\\[?(?:[0-9]+,)*[0-9]+\\]?$") THEN "integer_array"
+        ELSE "string"
+      END AS data_type
+      FROM
+        ${ctx.ref("events_" + params.eventSourceName)},
+        UNNEST(DATA) AS DATA
+      WHERE
+        DATE(occurred_at) = CURRENT_DATE - 1
+        AND event_type IN ("create_entity",
+          "update_entity",
+          "delete_entity",
+          "import_entity")
+        AND key NOT IN ("created_at","updated_at", "id")
+      ORDER BY
+        entity_table_name,
+        key)
+    GROUP BY entity_table_name,key
+  )
   GROUP BY
     entity_table_name)`)
 }
