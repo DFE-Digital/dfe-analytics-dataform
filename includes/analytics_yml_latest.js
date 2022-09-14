418ea93a397ac9e13a4e1a823015d84cc32d5571
Steven Legg <google-oauth2|105490946821392461153>
2022-09-14 15:25:17 +0000

    Allow external dependencies to be passed to dfe-analytics-dataform

--- includes/analytics_yml_latest.js
+++ includes/analytics_yml_latest.js
@@ -3,6 +3,7 @@ module.exports = (params) => {
     ...params.defaultConfig,
     type: "table",
     description: "Structured version of the latest version of the schema which dfe-analytics-dataform has been configured to process entity CRUD events from. This is NOT necessarily the same as the latest version in analytics.yml in the production Github repository.",
+    dependencies: params.dependencies,
     columns: {
       entity_name: "Name of the table we want entities to be streamed for.",
       description: "Description of this table to include in table metadata.",
