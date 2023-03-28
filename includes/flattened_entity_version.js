8d9718fab130ff9b89c5ed779e5475040241dfbb
Steven Legg <google-oauth2|105490946821392461153>
2023-03-28 08:41:42 +0000

    pastKeyNames bugfix

--- includes/flattened_entity_version.js
+++ includes/flattened_entity_version.js
@@ -82,10 +82,10 @@ module.exports = (params) => {
       else {
         var coalesceSql;
         if (!key.pastKeyNames) {
-          coalesceSql = `DATA_struct.${key.alias || key.keyName}`;
+          coalesceSql = `DATA_struct.${key.keyName}`;
         }
         else {
-          coalesceSql = `COALESCE(DATA_struct.${key.alias || key.keyName}, DATA_struct.${key.pastKeyNames.join(', DATA_struct.')})`;
+          coalesceSql = `COALESCE(DATA_struct.${key.keyName}, DATA_struct.${key.pastKeyNames.join(', DATA_struct.')})`;
         }
         var fieldSql;
         if(key.dataType == 'boolean') {
@@ -125,7 +125,7 @@ FROM (
             pastKeyNamesSql += `      ANY_VALUE(IF(key="${pastKeyName}",value,NULL)) AS ${pastKeyName},\n`;
             });
           }
-          return `ANY_VALUE(IF(key="${key.keyName}",value,NULL)) AS ${key.alias || key.keyName},\n` + pastKeyNamesSql;
+          return `ANY_VALUE(IF(key="${key.keyName}",value,NULL)) AS ${key.keyName},\n` + pastKeyNamesSql;
       }
     ).join('')
   }
