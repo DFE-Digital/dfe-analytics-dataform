00c18ac38b7385b0fdd0ae4acd541267f696ec89
Steven Legg <steven2.legg@education.gov.uk>
2024-05-20 12:12:36 +0000

    Only list 1 dfe-analytics configuration set per service instead of one per environment

--- includes/dfe_analytics_configuration.js
+++ includes/dfe_analytics_configuration.js
@@ -16,14 +16,13 @@ module.exports = (params) => {
     columns: {
       valid_from: "Timestamp at which this configuration of dfe-analytics began to apply.",
       valid_to: "Timestamp at which this configuration of dfe-analytics stopped applying. NULL if this is the current configuration.",
-      environment: "environment which this configuration of dfe-analytics applied to.",
       version: "The version of dfe-analytics that was used to stream events between these timestamps.",
       pseudonymise_web_request_user_id: "TRUE if request_user_id in " + params.bqEventsTableName + " was pseudonymised using SHA256 between these dates, or if it appears to be in the absence of an initialise_analytics event for this time period. FALSE if not."
     }
   }).query(ctx => `
     SELECT
       occurred_at AS valid_from,
-      FIRST_VALUE(occurred_at) OVER (PARTITION BY environment ORDER BY occurred_at ASC ROWS BETWEEN 1 FOLLOWING AND 1 FOLLOWING) AS valid_to,
+      FIRST_VALUE(occurred_at) OVER (ORDER BY occurred_at ASC ROWS BETWEEN 1 FOLLOWING AND 1 FOLLOWING) AS valid_to,
       environment,
       ${data_functions.eventDataExtract("data", "analytics_version")} AS version,
       CAST(JSON_VALUE(${data_functions.eventDataExtract("data", "config")}, "$.pseudonymise_web_request_user_id") AS BOOLEAN) AS pseudonymise_web_request_user_id
@@ -47,25 +46,6 @@ module.exports = (params) => {
         WHERE
           event_type = "initialise_analytics"
       ) AS valid_to,
-      (
-        SELECT
-          IF(
-            COUNT(DISTINCT environment) = 1,
-            ANY_VALUE(environment),
-            NULL)
-        FROM
-          ${"`" + params.bqProjectName + "." + params.bqDatasetName + "." + params.bqEventsTableName + "`"}
-        WHERE
-          occurred_at <
-            (
-              SELECT
-                MIN(occurred_at)
-              FROM
-                ${"`" + params.bqProjectName + "." + params.bqDatasetName + "." + params.bqEventsTableName + "`"}
-              WHERE
-                event_type = "initialise_analytics"
-            )
-      ) AS environment,
       CAST(NULL AS STRING) AS version,
       (
         SELECT
