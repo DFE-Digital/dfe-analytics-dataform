0eb37499f9d7ceca90ad41b660dc34d77978b465
Max Mills <8balldigitalsolutions@gmail.com>
2025-07-21 16:32:50 +0100

    more data functions

--- tests/data_functions.test.js
+++ tests/data_functions.test.js
@@ -5,7 +5,9 @@ const canonicalizeSQL = (sql) =>
     .replace(/\s+/g, ' ')
     .replace(/\s*\(\s*/g, '(')
     .replace(/\s*\)\s*/g, ')')
-    .trim();
+    .replace(/"{3}/g, '"')
+    .trim()
+    .toLowerCase();
 
 describe('stringToIntegerArray', () => {
   it('should generate the correct SQL query for a valid input string', () => {
@@ -317,5 +319,220 @@ describe('eventDataExtractIntegerArray', () => {
 
     const result = dataFunctions.eventDataExtract(dataField, keyToExtract, dynamic, 'integer_array', false);
     expect(canonicalizeSQL(result)).toEqual(canonicalizeSQL(expectedSQL));
+  });
+});
+
+describe('eventDataExtractListOfStringsBeginning', () => {
+  it('should return a SQL query to extract comma-separated values for keys starting with a specific prefix', () => {
+    const dataField = 'event_params';
+    const keyToExtractBegins = 'prefix_';
+    const dynamic = false;
+
+    const expectedSQL = `
+      NULLIF(
+        (SELECT IF(
+          ARRAY_LENGTH(ARRAY_CONCAT_AGG(value)) = 0,
+          NULL,
+          ARRAY_TO_STRING(ARRAY_CONCAT_AGG(value), ",")
+        ) AS concat_value
+        FROM UNNEST(event_params)
+        WHERE STARTS_WITH(key, "prefix_")),
+        ""
+      )
+    `;
+
+    const result = dataFunctions.eventDataExtractListOfStringsBeginning(dataField, keyToExtractBegins, dynamic);
+
+    expect(canonicalizeSQL(result)).toEqual(canonicalizeSQL(expectedSQL));
+  });
+
+  it('should return NULL if no keys match the prefix or all values are empty strings', () => {
+    const dataField = 'event_params';
+    const keyToExtractBegins = 'nonexistent_prefix_';
+    const dynamic = false;
+
+    const expectedSQL = `
+      NULLIF(
+        (SELECT IF(
+          ARRAY_LENGTH(ARRAY_CONCAT_AGG(value)) = 0,
+          NULL,
+          ARRAY_TO_STRING(ARRAY_CONCAT_AGG(value), ",")
+        ) AS concat_value
+        FROM UNNEST(event_params)
+        WHERE STARTS_WITH(key, "nonexistent_prefix_")),
+        ""
+      )
+    `;
+
+    const result = dataFunctions.eventDataExtractListOfStringsBeginning(dataField, keyToExtractBegins, dynamic);
+
+    expect(canonicalizeSQL(result)).toEqual(canonicalizeSQL(expectedSQL));
+  });
+});
+
+describe('eventDataExtractListOfStringsBeginning', () => {
+  it('should return a SQL query to extract comma-separated values for keys starting with a specific prefix', () => {
+    const dataField = 'event_params';
+    const keyToExtractBegins = 'prefix_';
+    const dynamic = false;
+
+    const expectedSQL = `
+      NULLIF(
+        (SELECT IF(
+          ARRAY_LENGTH(ARRAY_CONCAT_AGG(value)) = 0,
+          NULL,
+          ARRAY_TO_STRING(ARRAY_CONCAT_AGG(value), ",")
+        ) AS concat_value
+        FROM UNNEST(event_params)
+        WHERE STARTS_WITH(key, "prefix_")),
+        ""
+      )
+    `;
+
+    const result = dataFunctions.eventDataExtractListOfStringsBeginning(dataField, keyToExtractBegins, dynamic);
+
+    expect(canonicalizeSQL(result)).toEqual(canonicalizeSQL(expectedSQL));
+  });
+
+  it('should return NULL if no keys match the prefix or all values are empty strings', () => {
+    const dataField = 'event_params';
+    const keyToExtractBegins = 'nonexistent_prefix_';
+    const dynamic = false;
+
+    const expectedSQL = `
+      NULLIF(
+        (SELECT IF(
+          ARRAY_LENGTH(ARRAY_CONCAT_AGG(value)) = 0,
+          NULL,
+          ARRAY_TO_STRING(ARRAY_CONCAT_AGG(value), ",")
+        ) AS concat_value
+        FROM UNNEST(event_params)
+        WHERE STARTS_WITH(key, "nonexistent_prefix_")),
+        ""
+      )
+    `;
+
+    const result = dataFunctions.eventDataExtractListOfStringsBeginning(dataField, keyToExtractBegins, dynamic);
+
+    expect(canonicalizeSQL(result)).toEqual(canonicalizeSQL(expectedSQL));
+  });
+});
+
+describe('eventDataCreateOrReplace', () => {
+  it('should generate SQL to replace the value of an existing key in the event data', () => {
+    const eventData = [
+      { key: 'key1', value: 'value1' },
+      { key: 'key2', value: 'value2' },
+    ];
+    const keyToReplace = 'key1';
+    const newValue = 'newValue1';
+
+    const result = dataFunctions.eventDataCreateOrReplace(eventData, keyToReplace, newValue);
+
+    const expectedSQL = `
+      ARRAY_CONCAT(
+        ARRAY(
+          (
+            SELECT
+              AS STRUCT key,
+              value
+            FROM
+              UNNEST([object Object],[object Object])
+            WHERE
+              key != "key1"
+          )
+        ),
+        [
+          STRUCT(
+            "key1" AS key,
+            ["newValue1"] AS value
+          )
+        ]
+      )
+    `;
+
+    expect(canonicalizeSQL(result)).toEqual(canonicalizeSQL(expectedSQL));
+  });
+});
+
+describe('wait', () => {
+  test('should generate correct SQL for a valid interval string', () => {
+    const intervalString = '1 SECOND';
+    const result = dataFunctions.wait(intervalString);
+    const expectedSQL = `BEGIN
+    DECLARE WAIT STRING;
+    DECLARE DELAY_TIME DATETIME;
+    SET WAIT = 'TRUE';
+    SET DELAY_TIME = DATETIME_ADD(CURRENT_DATETIME, INTERVAL ${intervalString});
+    WHILE WAIT = 'TRUE' DO
+      IF (DELAY_TIME < CURRENT_DATETIME) THEN
+        SET WAIT = 'FALSE';
+      END IF;
+    END WHILE;
+  END;`;
+    expect(canonicalizeSQL(result)).toBe(canonicalizeSQL(expectedSQL));
+  });
+});
+
+describe('setKeyConstraints', () => {
+  test('setKeyConstraints generates correct SQL for primary and foreign keys', () => {
+    const ctx = {
+      self: () => '`project.dataset.table`',
+      name: () => 'table',
+      ref: (table) => `\`project.dataset.${table}\``
+    };
+
+    const dataform = {
+      projectConfig: {
+        defaultDatabase: 'project',
+        defaultSchema: 'dataset',
+        schemaSuffix: ''
+      }
+    };
+
+    const constraints = {
+      primaryKey: 'myidname',
+      foreignKeys: [
+        { keyInThisTable: 'myotherkeyname', foreignTable: 'myothertable', keyInForeignTable: 'myotheridname' },
+        { keyInThisTable: 'anotherkeyname', foreignTable: 'myotherothertable' },
+        {
+          keyInThisTable: 'firstpartofcompositekey, secondpartofcompositekey',
+          foreignTable: 'myothertable',
+          keyInForeignTable: 'firstpartofcompositekeyinothertable, secondpartofcompositekeyinothertable'
+        }
+      ]
+    };
+
+    const result = dataFunctions.setKeyConstraints(ctx, dataform, constraints);
+
+    const expectedSQL = `
+      IF TRUE THEN
+      /* Delete all key constraints on the table - even ones that are no longer included in dfeAnalyticsDataform() configuration */
+        ALTER TABLE \`project.dataset.table\` DROP PRIMARY KEY IF EXISTS;
+        FOR constraint_to_drop IN (
+          SELECT
+            SPLIT(constraint_name, ".")[1]
+          FROM
+            \`project.dataset.INFORMATION_SCHEMA.TABLE_CONSTRAINTS\`
+          WHERE
+            constraint_type = "FOREIGN KEY"
+            AND table_name = "table"
+          )
+        DO
+          ALTER TABLE \`project.dataset.table\` DROP CONSTRAINT IF EXISTS constraint_to_drop;
+          ${dataFunctions.wait("2 SECOND")}
+        END FOR;
+      /* Set primary key */
+        ALTER TABLE \`project.dataset.table\` ADD PRIMARY KEY(myidname) NOT ENFORCED;
+        ${dataFunctions.wait("2 SECOND")}
+      /* Set foreign key constraints */
+        ALTER TABLE \`project.dataset.table\`
+            ADD CONSTRAINT myotherkeyname_relationship FOREIGN KEY(myotherkeyname) REFERENCES \`project.dataset.myothertable\`(myotheridname) NOT ENFORCED,
+            ADD CONSTRAINT anotherkeyname_relationship FOREIGN KEY(anotherkeyname) REFERENCES \`project.dataset.myotherothertable\`(id) NOT ENFORCED,
+            ADD CONSTRAINT firstpartofcompositekey_secondpartofcompositekey_relationship FOREIGN KEY(firstpartofcompositekey, secondpartofcompositekey) REFERENCES \`project.dataset.myothertable\`(firstpartofcompositekeyinothertable, secondpartofcompositekeyinothertable) NOT ENFORCED;
+        END IF;
+    `;
+
+    expect(canonicalizeSQL(result)).toBe(canonicalizeSQL(expectedSQL));
   });
 });
