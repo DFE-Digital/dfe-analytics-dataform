574c7d1669c8a4a4badb126e0f0a0442c60a056b
elizabeth.karina@education.gov.uk <elizabeth.karina@education.gov.uk>
2026-03-11 11:32:26 +0000

    Add test file

---
+++ definitions/bat_register_airbyte_test.js
@@ -0,0 +1,102 @@
+/* Test definition for Airbyte dual-run with data. Reads from rtt_airbyte_production (Airbyte source)
+   NOTE: This runs ALONGSIDE the existing dfe-analytics pipeline, not instead of it.
+*/
+
+const dfeAnalyticsDataform = require("../");
+
+dfeAnalyticsDataform({
+    disabled: true,
+    // --- Existing dfe-analytics config (keep this for dual-run) ---
+    transformEntityEvents: true,
+    eventSourceName: "register",
+    bqDatasetName: "register_events_production",
+    bqEventsTableName: "events",    
+    enableSessionTables: false,
+    enableSessionDetailsTable: false,
+    enableMonitoring: false,
+    expirationDays: false,
+    
+    // --- NEW: Enable Airbyte ---
+    enableAirbyteSource: true,
+    
+    airbyteConfig: {
+        datasetName: "rtt_airbyte_production",
+        tablePrefix: "",                        
+        outputSuffix: "_airbyte",               
+        primaryKeyField: "id",                   
+        changeDetectionStrategy: "content_hash",
+    },
+    
+    airbyteEnableVersioning: true,
+    airbyteEnableFieldUpdates: true,
+    airbyteEnableAssertions: true,
+    
+    // --- Data schema (start with a small subset for testing!) ---
+    dataSchema: [{
+            entityTableName: "academic_cycles",
+            description: "",
+            keys: [{
+                keyName: "start_date",
+                dataType: "date",
+                description: "Start date of academic cycle"
+            }, {
+                keyName: "end_date",
+                dataType: "date",
+                description: "End date of academic cycle"
+            }]
+        },
+        {
+            entityTableName: "activities",
+            description: "",
+            materialisation: "view",
+            keys: [{
+                keyName: "action_name",
+                dataType: "string",
+                description: "action activity applies to"
+            }, {
+                keyName: "controller_name",
+                dataType: "string",
+                description: "controller activity applies to"
+            }, {
+                keyName: "metadata",
+                dataType: "string",
+                description: "contains action, format, subject, controller & training_route"
+            }, {
+                keyName: "user_id",
+                dataType: "string",
+                description: "",
+                foreignKeyTable: "users"
+            }]
+        },
+        {
+            entityTableName: "allocation_subjects",
+            description: "",
+            keys: [{
+                keyName: "name",
+                dataType: "string",
+                description: "Name of the allocation subject"
+            }]
+        },
+        {
+            entityTableName: "apply_applications",
+            description: "Link between a trainee and an application form in Apply, if and only if a SCITT successfully imported data from Apply when they started registering this trainee in Register. Because of this this table is *not* suitable for use to join all trainees in Register on to corresponding candidates/application forms in Apply.",
+            keys: [{
+                keyName: "accredited_body_code",
+                dataType: "string",
+                description: "accredited body code for the Apply application"
+            }, {
+                keyName: "recruitment_cycle_year",
+                dataType: "integer",
+                description: "recruitment cycle year the application relates to"
+            }, {
+                keyName: "state",
+                dataType: "string",
+                description: "state code for the application"
+            }, {
+                keyName: "apply_id",
+                dataType: "string",
+                description: "Application form ID in Apply, if and only if a SCITT successfully imported data from Apply when they started registering this trainee in Register. Because of this this field is *not* suitable for use to join all trainees in Register on to corresponding candidates/application forms in Apply."
+            }]
+        }
+    ]
+});
